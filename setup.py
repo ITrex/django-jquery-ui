80616093be04c7884c76e3bef6f23f03a3330ac6
AleXeY989 <alex1chupahin@ya.ru>
2014-07-03 16:21:33 +0400

    four

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='django-jquery-ui',
     version='1.11.0',
-    url='https://github.com/AleXeY989/django_jquery2.git',
+    url='https://github.com/AleXeY989/django_jquery_ui.git',
     description='Django package for jquery-ui: jQuery UI is a curated set of user interface interactions, effects, widgets, and themes built on top',
     author='Paul Bakaus',
     maintainer='AleXeY989',
@@ -23,5 +23,5 @@ setup(
         'Topic :: Utilities',
     ],
     packages=['django_jquery_ui'],
-    package_data={'django_jquery_ui': ['static/js/django_jquery_ui/*.js', 'static/css/django_jquery_ui/*.css', 'static/img/django_jquery_ui/*.png', 'static/img/django_jquery_ui/*.gif']}
+    package_data={'django_jquery_ui': ['static/django_jquery_ui/js/*.js', 'static/django_jquery_ui/css/*.css', 'static/django_jquery_ui/img/*.png', 'static/django_jquery_ui/img/*.gif']}
 )
